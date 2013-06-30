fd99f3ecaea32d29f38d6347b7a4a0f24e07dc76
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2013-06-29 23:00:17 -0300

    split test files

---
+++ spec/walky/walker_spec.rb
@@ -0,0 +1,59 @@
+require 'walky'
+
+module Walky
+  describe Walker do
+    let(:lcd) { { "eletronics" => { "tv" => { "screen" => "LCD", "digital" => true } } } }
+    let(:led) { { "eletronics" => { "tv" => { "screen" => "LED", "digital" => false } } } }
+    let(:plasm) { { "eletronics" => { "tv" => { "screen" => "PLASM", "digital" => true } } } }
+
+    let(:lcd_walky) { Walky::Walker.new(lcd) }
+    let(:lcd_screen) { lcd["eletronics"]["tv"]["screen"] }
+
+    describe '#[]' do
+      it 'parse hash values' do
+        lcd_walky["eletronics"].should be_kind_of(Hash)
+      end
+
+      it "parse two level path" do
+        lcd_walky["eletronics tv"].should be_kind_of(Hash)
+      end
+
+      it "parse last level path" do
+        lcd_walky["eletronics tv screen"].should == lcd_screen
+      end
+    end
+
+    describe '#walk' do
+      it 'parse hash values' do
+        lcd_walky.walk("eletronics tv screen").should == lcd_screen
+      end
+    end
+
+    describe '#same_path' do
+      it 'access two hash with same path at same time' do
+        lcd_walky["eletronics tv"].same_path(led).should be_kind_of(Array)
+        lcd_walky["eletronics tv"].same_path(led)[0].should == lcd_walky["eletronics tv"]
+        lcd_walky["eletronics tv"].same_path(led)[1].should == led["eletronics"]["tv"]
+      end
+
+      it 'access multiple hashes with same path at same time' do
+        walked = lcd_walky["eletronics tv"].same_path(led, plasm)
+        walked.size.should == 3
+        walked[0].should == lcd_walky["eletronics tv"]
+        walked[1].should == led["eletronics"]["tv"]
+        walked[2].should == plasm["eletronics"]["tv"]
+      end
+
+      it 'can also access sub paths from all related hashes' do
+        walked = lcd_walky["eletronics tv"].same_path(led, plasm).all do |a, b, c|
+          a["screen"].should == "LCD"
+          b["screen"].should == "LED"
+          c["screen"].should == "PLASM"
+
+          "#{a["screen"]}#{b["screen"]}#{c["screen"]}"
+        end
+        walked.should == "LCDLEDPLASM"
+      end
+    end
+  end
+end
--- spec/walky_spec.rb
+++ spec/walky_spec.rb
@@ -1,10 +1,7 @@
 require 'walky'
 
 describe Walky do
-
   let(:lcd) { { "eletronics" => { "tv" => { "screen" => "LCD", "digital" => true } } } }
-  let(:led) { { "eletronics" => { "tv" => { "screen" => "LED", "digital" => false } } } }
-  let(:plasm) { { "eletronics" => { "tv" => { "screen" => "PLASM", "digital" => true } } } }
 
   let(:lcd_walky) { Walky::Walker.new(lcd) }
 
@@ -43,6 +40,14 @@ describe Walky do
       Walky.move(command, ":type").should == "ls"
       Walky.move(command, ":params").should == ["l", "a"]
     end
+
+    it 'ensure that can navigate when iterating' do
+      @collection["menu items"].each do |item|
+        Walky.move(item, "cat keywords").should be_kind_of(Array)
+      end
+
+      Walky.move(@collection["menu items"].first, "cat keywords").size.should == 2
+    end
   end
 
   describe '.extract' do
@@ -77,73 +82,6 @@ describe Walky do
       keys = Walky.extract_with_sym(print, "file")
       keys[:type].should == "A4"
       keys[:password].should == "secret"
-    end
-  end
-
-  describe Walky::Walker do
-    let(:lcd_screen) { lcd["eletronics"]["tv"]["screen"] }
-
-    describe '#[]' do
-      it 'parse hash values' do
-        lcd_walky["eletronics"].should be_kind_of(Hash)
-      end
-
-      it "parse two level path" do
-        lcd_walky["eletronics tv"].should be_kind_of(Hash)
-      end
-
-      it "parse last level path" do
-        lcd_walky["eletronics tv screen"].should == lcd_screen
-      end
-    end
-
-    describe '#walk' do
-      it 'parse hash values' do
-        lcd_walky.walk("eletronics tv screen").should == lcd_screen
-      end
-    end
-
-    describe '#same_path' do
-      it 'access two hash with same path at same time' do
-        lcd_walky["eletronics tv"].same_path(led).should be_kind_of(Array)
-        lcd_walky["eletronics tv"].same_path(led)[0].should == lcd_walky["eletronics tv"]
-        lcd_walky["eletronics tv"].same_path(led)[1].should == led["eletronics"]["tv"]
-      end
-
-      it 'access multiple hashes with same path at same time' do
-        walked = lcd_walky["eletronics tv"].same_path(led, plasm)
-        walked.size.should == 3
-        walked[0].should == lcd_walky["eletronics tv"]
-        walked[1].should == led["eletronics"]["tv"]
-        walked[2].should == plasm["eletronics"]["tv"]
-      end
-
-      it 'can also access sub paths from all related hashes' do
-        walked = lcd_walky["eletronics tv"].same_path(led, plasm).all do |a, b, c|
-          a["screen"].should == "LCD"
-          b["screen"].should == "LED"
-          c["screen"].should == "PLASM"
-
-          "#{a["screen"]}#{b["screen"]}#{c["screen"]}"
-        end
-        walked.should == "LCDLEDPLASM"
-      end
-    end
-
-    describe '#each' do
-      it 'ensure that each work properly' do
-        @collection["menu items"].each do |item|
-          item["item"].should_not be_nil
-        end
-      end
-
-      it 'ensure that can navigate when iterating' do
-        @collection["menu items"].each do |item|
-          Walky.move(item, "cat keywords").should be_kind_of(Array)
-        end
-
-        Walky.move(@collection["menu items"].first, "cat keywords").size.should == 2
-      end
     end
   end
 end
