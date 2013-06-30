a6b2a63327fb892e70131d39db7ab750c9a85585
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2013-06-29 23:14:56 -0300

    remove unnecessary module calls

--- lib/walky.rb
+++ lib/walky.rb
@@ -5,19 +5,18 @@ require 'walky/instance_methods'
 
 module Walky
   def self.move(hash, path)
-    Walky::Parser.parse(hash, path)
+    Parser.parse(hash, path)
   end
 
   def self.extract(hash, path)
-    Walky::Parser.extract(hash, path)
+    Parser.extract(hash, path)
   end
 
   def self.extract_with_sym(hash, path)
-    Walky::Parser.extract_with_sym(hash, path)
+    Parser.extract_with_sym(hash, path)
   end
 
   class Walker
-
     def initialize(hash)
       @hash = hash
     end
@@ -27,11 +26,11 @@ module Walky
     end
 
     def self.parse(hash, path)
-      Walky::Parser.parse(hash, path)
+      Parser.parse(hash, path)
     end
 
     def extract(path)
-      Walky::Parser.extract(@hash, path)
+      Parser.extract(@hash, path)
     end
 
     alias_method :[], :parse
